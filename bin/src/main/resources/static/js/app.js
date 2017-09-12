34b8794d605ad674bfadf57aed85dd302cbbe596
diego <diego@diego-VirtualBox>
2017-09-12 17:18:22 -0300

    teste

--- bin/src/main/resources/static/js/app.js
+++ bin/src/main/resources/static/js/app.js
@@ -5,6 +5,7 @@ appCliente.config(function($routeProvider, $locationProvider) {
 	.when("/clientes", {templateUrl: 'view/cliente.html', controller: 'clienteController'})
 	.when("/cidades", {templateUrl: 'view/cidade.html', controller: 'cidadeController'})
 	.when("/estados", {templateUrl: 'view/estado.html', controller: 'estadoController'})
+	.when("/veiculos", {templateUrl: 'view/veiculo.html', controller: 'veiculoController'})
 	.otherwise({rediretTo: '/'});
 	
 	$locationProvider.html5Mode(true);
